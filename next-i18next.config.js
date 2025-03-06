3ee65c7e8106b4ddf8ecc4eb8716c3ed797cdd72
oZeca <ze.lupicaetano@gmail.com>
2025-03-06 15:25:10 +0000

    adds next-i18next configs

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,6 +4,4 @@ module.exports = {
     locales: ['en', 'es', 'fr'],
     localeDetection: true,
   },
-  localePath: './src/locales',
-  reloadOnPrerender: process.env.NODE_ENV === 'development',
-} 
+}
