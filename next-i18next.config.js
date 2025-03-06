150824d38b8ff72a7e7100ba3426c2102def5453
oZeca <ze.lupicaetano@gmail.com>
2025-03-06 18:31:52 +0000

    feat: Update next-i18next configuration with localePath and reloadOnPrerender

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,4 +4,6 @@ module.exports = {
     locales: ['en', 'es', 'fr'],
     localeDetection: true,
   },
+  localePath: './src/locales',
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
 }
