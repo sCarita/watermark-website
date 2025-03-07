2e755aafdf730d0d2d34d73a86e3fdcde6a8c1a6
oZeca <ze.lupicaetano@gmail.com>
2025-03-07 11:01:50 +0000

    fix: unused files js to ts

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,23 @@
 import { NextRequest, NextResponse } from 'next/server'
 import acceptLanguage from 'accept-language'
-import { i18n } from './next-i18next.config'
+import i18nConfig from './next-i18next.config'
 
-acceptLanguage.languages(i18n.locales)
+acceptLanguage.languages(i18nConfig.i18n.locales)
 
 export const config = {
   // matcher: '/:lng*'
-  matcher: ['/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js).*)']
+  matcher: ['/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js).*)'],
 }
 
 const cookieName = 'i18nextLng'
 
 export function middleware(req: NextRequest) {
   // Check if there is a cookie with the user's preferred language
-  let lng: string = i18n.defaultLocale
-  
+  let lng: string = i18nConfig.i18n.defaultLocale
+
   if (req.cookies.has(cookieName)) {
     const cookieValue = req.cookies.get(cookieName)?.value
-    if (cookieValue && i18n.locales.includes(cookieValue)) {
+    if (cookieValue && i18nConfig.i18n.locales.includes(cookieValue)) {
       lng = cookieValue
     }
   } else {
@@ -41,11 +41,14 @@ export function middleware(req: NextRequest) {
 
   // Create a response object
   const response = NextResponse.next()
-  
+
   // Set the language cookie if it doesn't exist or is different
-  if (!req.cookies.has(cookieName) || req.cookies.get(cookieName)?.value !== lng) {
+  if (
+    !req.cookies.has(cookieName) ||
+    req.cookies.get(cookieName)?.value !== lng
+  ) {
     response.cookies.set(cookieName, lng)
   }
 
   return response
-} 
+}
--- next-i18next.config.ts
+++ next-i18next.config.ts
@@ -1,9 +1,13 @@
-module.exports = {
+import type { UserConfig } from 'next-i18next'
+
+const i18nConfig: UserConfig = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es', 'fr'],
-    localeDetection: true,
+    localeDetection: false,
   },
   localePath: './src/locales',
   reloadOnPrerender: process.env.NODE_ENV === 'development',
 }
+
+export default i18nConfig
